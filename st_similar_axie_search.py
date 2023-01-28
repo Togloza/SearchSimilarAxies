55974fafc552072ed498358e773d9012c4f03084
Togloza <120382975+Togloza@users.noreply.github.com>
2023-01-27 18:05:38 -0800

    Introduced undercut feature

--- st_similar_axie_search.py
+++ st_similar_axie_search.py
@@ -36,7 +36,21 @@ def get_axie_data(axie_id, filters = set()):
 
     
     search_url = f"https://app.axieinfinity.com/marketplace/axies/?partTypes=Tail&auctionTypes=Sale&{'&'.join(search_parts)}"
-    return search_url
+    
+    
+        ql_endpoint = 'https://graphql-gateway.axieinfinity.com/graphql'
+    payload = {"operationName":"GetAxieBriefList","variables":{"from":0,"sort":"PriceAsc","size":24,"auctionType":"Sale","criteria":{"bodyShapes":None,"breedCount":None,"classes":None,"numJapan":None,"numMystic":None,"numShiny":None,"numSummer":None,"numXmas":None,"parts":['horn-' + parts['horn'],'eyes-' + parts['eyes'],'ears-' + parts['ears'], 'mouth-' + parts['mouth'], 'back-' + parts['back'], 'tail-' + parts['tail']],"ppAquatic":None,"ppBeast":None,"ppBird":None,"ppBug":None,"ppDawn":None,"ppDusk":None,"ppMech":None,"ppPlant":None,"ppReptile":None,"pureness":None,"purity":None,"stages":None,"title":None}},"query":"query GetAxieBriefList($auctionType: AuctionType, $criteria: AxieSearchCriteria, $from: Int, $sort: SortBy, $size: Int, $owner: String) {\n axies(\n auctionType: $auctionType\n criteria: $criteria\n from: $from\n sort: $sort\n size: $size\n owner: $owner\n ) {\n total\n results {\n ...AxieBrief\n __typename\n }\n __typename\n }\n}\n\nfragment AxieBrief on Axie {\n id\n name\n stage\n class\n breedCount\n image\n title\n genes\n newGenes\n battleInfo {\n banned\n __typename\n }\n order {\n id\n currentPrice\n currentPriceUsd\n __typename\n }\n parts {\n id\n name\n class\n type\n specialGenes\n __typename\n }\n __typename\n}\n"}
+    response = requests.post(ql_endpoint, json=payload)
+    
+    if response.status_code == 200:
+        data = json.loads(response.content.decode('utf-8'))
+        ids = [axie['id'] for axie in data['data']['axies']['results']]
+        price = [axie['order']['currentPriceUsd'] for axie in data['data']['axies']['results']]
+        combined_data = [{"id": id, "price": price} for id, price in zip(ids, price)]
+    else:
+        print('Error:', response.status_code)
+    
+    return search_url, combined_data
 
 filter_options = ["Horn", "Eyes", "Ears", "Mouth", "Back", "Tail"]
 
@@ -48,12 +62,17 @@ st.title("Find Similar Axies by ID")
 axie_id = st.text_input("Enter Axie ID:")
 url = "https://app.axieinfinity.com/marketplace/axies/"
 if axie_id:
-    url = get_axie_data(axie_id, filters)
+    url, price_data = get_axie_data(axie_id, filters)
     st.markdown(f"""
         <a href="{url}" target="_blank">Search Marketplace {axie_id}</a>
         """, unsafe_allow_html=True)
-        #st.markdown(url, unsafe_allow_html=Truse)
+    st.write("Lowest Priced Axie")
+    st.write(price_data[0])
+    if price_data[0]['id'] != axie_id:
+        st.write("Undercut!")
 
+        
+st.write("Change Log: Added feature to see if the axie has been undercut on the market")  
 st.write("Change Log: Added option to exclude parts in the sidebar")        
 st.write("This app is useful for determining an axie's list price based on similar axies on the market. This app removes the need to filter the market by each part individually. Still a work in progress, more functionality to be added soon.")
 
